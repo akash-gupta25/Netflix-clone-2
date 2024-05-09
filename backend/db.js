4c5f404422a57faeb3bf2973335db2e781be6620
akash-gupta25 <107639829+akash-gupta25@users.noreply.github.com>
2024-05-09 23:09:15 +0530

    db test connection

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
     mongoose.set('strictQuery', false);
     mongoose
       // .connect("mongodb+srv://akashmovies:3CEx07fwiQpZYnDO@cluster0.cdbkvld.mongodb.net/")
-      .connect("mongodb+srv://akashmovies:3CEx07fwiQpZYnDO@ac-djfb2od-shard-00-02.cdbkvld.mongodb.net:27017/netflix-clone?retryWrites=true&w=majority&appName=Cluster0")
+      // .connect("mongodb+srv://akashmovies:3CEx07fwiQpZYnDO@cluster0.cdbkvld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+      .connect("mongodb+srv://akashmovies:3CEx07fwiQpZYnDO@cluster0.cdbkvld.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0")
       .then(() => {
         console.log("DB Connetion Successfull");
       })
