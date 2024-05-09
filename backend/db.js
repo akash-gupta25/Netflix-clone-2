e34997e93613c77f362c27b2e84454f09ca2633d
akash-gupta25 <107639829+akash-gupta25@users.noreply.github.com>
2024-05-09 23:13:45 +0530

    Db url change

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
     mongoose
       // .connect("mongodb+srv://akashmovies:3CEx07fwiQpZYnDO@cluster0.cdbkvld.mongodb.net/")
       // .connect("mongodb+srv://akashmovies:3CEx07fwiQpZYnDO@cluster0.cdbkvld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-      .connect("mongodb+srv://akashmovies:3CEx07fwiQpZYnDO@cluster0.cdbkvld.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0")
+      .connect("mongodb+srv://akashmovies:3CEx07fwiQpZYnDO@cluster0.cdbkvld.mongodb.net:27017/test?retryWrites=true&w=majority&appName=Cluster0")
       .then(() => {
         console.log("DB Connetion Successfull");
       })
