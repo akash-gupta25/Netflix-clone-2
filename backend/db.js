796f46c4491a34fae5baf01a596b8789c36af4ae
akash-gupta25 <107639829+akash-gupta25@users.noreply.github.com>
2024-05-10 10:15:34 +0530

    DB URL change

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 // const DbUrl = "mongodb+srv://akashmovies:3CEx07fwiQpZYnDO@cluster0.cdbkvld.mongodb.net/";
 // const DbUrl = "mongodb+srv://akashmovies:3CEx07fwiQpZYnDO@cluster0.cdbkvld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const DbUrl = "mongodb+srv://akashmovies:3CEx07fwiQpZYnDO@cluster0.cdbkvld.mongodb.net:27017/test";
+// const DbUrl = "mongodb+srv://akashmovies:3CEx07fwiQpZYnDO@cluster0.cdbkvld.mongodb.net:27017/test";
+const DbUrl = "mongodb+srv://<username:password>@cluster0.cdbkvld.mongodb.net:27017/netflix-clone?retryWrites=true&w=majority&appName=Cluster0";
+
 
  exports.Database= function Database() {
     mongoose.set('strictQuery', false);
