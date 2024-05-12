8ff59434d5d0860e31ca0d2cdd0b3f41dd1dca20
akash-gupta25 <107639829+akash-gupta25@users.noreply.github.com>
2024-05-12 10:19:40 +0530

    db change 8

--- backend/db.js
+++ backend/db.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 // const DbUrl = "mongodb+srv://akashmovies:3CEx07fwiQpZYnDO@cluster0.cdbkvld.mongodb.net:27017/netflix-clone?retryWrites=true&w=majority&appName=Cluster0";
 // const DbUrl = "mongodb+srv://akashmovies:3CEx07fwiQpZYnDO@cluster0.cdbkvld.mongodb.net:27017/netflix-clone?retryWrites=true&w=majority";
 // const uri = "mongodb+srv://akashmovies:3CEx07fwiQpZYnDO@cluster0.cdbkvld.mongodb.net/netflix-clone?retryWrites=true&w=majority&appName=Cluster0";
-const uri = "mongodb+srv://akashmovies:3CEx07fwiQpZYnDO@cluster0.cdbkvld.mongodb.net:27017/netflix-clone?ssl=true&authSource=admin&retryWrites=true&w=majority";
+const uri = "mongodb://akashmovies:3CEx07fwiQpZYnDO@cluster0.cdbkvld.mongodb.net:27017/netflix-clone?ssl=true&authSource=admin&retryWrites=true&w=majority";
 const clientOptions = {useNewUrlParser: true,useUnifiedTopology: true,};
 
 
