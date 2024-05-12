8d54ca0a7b02f8663faecd5b1dbcda7aae46f860
akash-gupta25 <107639829+akash-gupta25@users.noreply.github.com>
2024-05-12 10:13:44 +0530

    db change 6

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,8 @@ const mongoose = require("mongoose");
 // const DbUrl = "mongodb+srv://akashmovies:3CEx07fwiQpZYnDO@cluster0.cdbkvld.mongodb.net:27017/test";
 // const DbUrl = "mongodb+srv://akashmovies:3CEx07fwiQpZYnDO@cluster0.cdbkvld.mongodb.net:27017/netflix-clone?retryWrites=true&w=majority&appName=Cluster0";
 // const DbUrl = "mongodb+srv://akashmovies:3CEx07fwiQpZYnDO@cluster0.cdbkvld.mongodb.net:27017/netflix-clone?retryWrites=true&w=majority";
-const uri = "mongodb+srv://akashmovies:3CEx07fwiQpZYnDO@cluster0.cdbkvld.mongodb.net/netflix-clone?retryWrites=true&w=majority&appName=Cluster0";
+// const uri = "mongodb+srv://akashmovies:3CEx07fwiQpZYnDO@cluster0.cdbkvld.mongodb.net/netflix-clone?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://akashmovies:3CEx07fwiQpZYnDO@cluster0.cdbkvld.mongodb.net:27017/netflix-clone?ssl=true&authSource=admin&retryWrites=true&w=majority";
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
 
